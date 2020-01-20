0d701a46ead00ac0065d10a8ff7062c1ab39a2f8
Ajinkya Kale <kaleajinkya@gmail.com>
2020-01-20 12:19:13 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setuptools.setup(
      description="Scene Graph Parse",
      url="https://github.com/vacancy/SceneGraphParser",
      packages=setuptools.find_packages(),
+     package_data={'sng_parser': 'sng_parser/_data'}
      install_requires=['tabulate']
  )
